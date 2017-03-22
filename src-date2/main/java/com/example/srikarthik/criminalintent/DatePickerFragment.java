1a60133983b67c36d8c07c92e25c701363e77394
Karthik1192 <mrkarthik1992@gmail.com>
2017-03-22 14:52:32 +0530

    date-2

---
+++ src-date2/main/java/com/example/srikarthik/criminalintent/DatePickerFragment.java
@@ -0,0 +1,26 @@
+package com.example.srikarthik.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * Created by Biswin on 3/22/2017.
+ */
+public class DatePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
